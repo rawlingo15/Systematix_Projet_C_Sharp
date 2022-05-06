f7d71ad4fa04f3f8bb5d25f7297c3f939e2c6ed4
Flavio Vasquez <48759611+rawlingo15@users.noreply.github.com>
2022-05-06 14:58:20 -0400

    Update 2022-05-06
    Weekly update

--- LesBlaguesDeFlavio/LesBlaguesDeFlavio/Controllers/BlaguesController.cs
+++ LesBlaguesDeFlavio/LesBlaguesDeFlavio/Controllers/BlaguesController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LesBlaguesDeFlavio.Data;
 using LesBlaguesDeFlavio.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace LesBlaguesDeFlavio.Controllers
 {
@@ -24,6 +25,18 @@ namespace LesBlaguesDeFlavio.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Blague.ToListAsync());
+        }
+
+        // GET: Blagues/SearchForm
+        public async Task<IActionResult> SearchForm()
+        {
+            return View();
+        }
+
+        // POST: Blagues/SearchResults
+        public async Task<IActionResult> SearchResults(string MotsCles)
+        {
+            return View("Index", await _context.Blague.Where(b => b.Question.Contains(MotsCles)).ToListAsync());
         }
 
         // GET: Blagues/Details/5
@@ -45,6 +58,7 @@ namespace LesBlaguesDeFlavio.Controllers
         }
 
         // GET: Blagues/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -53,6 +67,7 @@ namespace LesBlaguesDeFlavio.Controllers
         // POST: Blagues/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Question,Reponse")] Blague blague)
@@ -67,6 +82,7 @@ namespace LesBlaguesDeFlavio.Controllers
         }
 
         // GET: Blagues/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -85,6 +101,7 @@ namespace LesBlaguesDeFlavio.Controllers
         // POST: Blagues/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Question,Reponse")] Blague blague)
@@ -118,6 +135,7 @@ namespace LesBlaguesDeFlavio.Controllers
         }
 
         // GET: Blagues/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -136,6 +154,7 @@ namespace LesBlaguesDeFlavio.Controllers
         }
 
         // POST: Blagues/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
